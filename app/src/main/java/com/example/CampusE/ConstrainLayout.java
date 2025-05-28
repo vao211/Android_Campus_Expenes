77b4d8f6974ee1e0cd0448e02bdcaa9ac1e067da
vao211 <vaolu211@gmail.com>
2025-05-28 23:27:07 +0700

    add: app campus layout

---
+++ app/src/main/java/com/example/CampusE/ConstrainLayout.java
@@ -0,0 +1,14 @@
+package com.example.CampusE;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConstrainLayout extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrain_layout);
+    }
+}
